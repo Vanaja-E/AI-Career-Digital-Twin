0a59ed28c22ebd98f2d969b251c53743449c0dfa
Vanaja-E <vanajae850@gmail.com>
2026-07-22 20:27:28 +0530

    In resume one colume added

--- backend/app/models.py
+++ backend/app/models.py
@@ -1,4 +1,6 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey, Text
+from sqlalchemy.orm import relationship
+
 from app.database import Base
 
 
@@ -9,3 +11,27 @@ class User(Base):
     full_name = Column(String(100), nullable=False)
     email = Column(String(100), unique=True, nullable=False)
     password = Column(String(255), nullable=False)
+
+    resumes = relationship("Resume", back_populates="user")
+
+
+class Resume(Base):
+    __tablename__ = "resumes"
+
+    id = Column(Integer, primary_key=True, index=True)
+
+    user_id = Column(Integer, ForeignKey("users.id"))
+
+    filename = Column(String(255))
+
+    resume_text = Column(Text)
+
+    ats_score = Column(Integer)
+
+    skills = Column(Text)
+
+    suggestions = Column(Text)
+
+    summary = Column(Text)
+
+    user = relationship("User", back_populates="resumes")
